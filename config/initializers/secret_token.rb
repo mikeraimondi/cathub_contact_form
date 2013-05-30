6f3d4562658037bbd5f251ef0ffa03d88849f9a1
Michael Raimondi <zeno@pobox.com>
2013-05-30 09:02:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CathubContactForm::Application.config.secret_token = 'bf697907e0c7d2afa36dca767840497ffd00e885c689a38ad0a58f8d0508dc1a1fbd46ad5ad82502153e4dd0fb1d388523f70d71a8f73131cd306cd84d3945a5'
